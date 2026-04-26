96cbadb16ee35c5557e5eee75faf6f8767de3261
Felipe-Clemente <felipeclementej2@gmail.com>
2026-04-26 09:20:52 -0300

    feat: Initial commit

---
+++ Unidade II/atv06_react-ToDoList/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const Container = () => {
+  return(
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <h1>To-do List</h1>
+      <h2>Organize suas tarefas</h2>
+
+
+    </>
+  )
+}
